a0b0d555d2ef2dc578af3987dd4fbd58cae78958
Shiva Nathan <16snathan@gmail.com>
2019-11-30 19:48:53 -0500

    Created new pickle containing training/validation/test images, now scaled up to 84x84 and converted to RGB

--- keras-knowledge-distillation/convert_csv_to_numpy_array.py
+++ keras-knowledge-distillation/convert_csv_to_numpy_array.py
@@ -20,10 +20,12 @@ def save_image_data():
     imDimensions = (height,width)
     scaledHeight = 84
     scaledWidth = 84
+    channels = 3
     scaledDimensions = (scaledHeight,scaledWidth)
-    trainDimensions = (num_train,scaledHeight,scaledWidth)
-    valDimensions = (num_val,scaledHeight,scaledWidth)
-    testDimensions = (num_test,scaledHeight,scaledWidth)
+    # image data format of samples,channels,height,width
+    trainDimensions = (num_train,channels,scaledHeight,scaledWidth)
+    valDimensions = (num_val,channels,scaledHeight,scaledWidth)
+    testDimensions = (num_test,channels,scaledHeight,scaledWidth)
     # Initialize the output arrays
     x_train = np.empty(trainDimensions,dtype='uint8')
     y_train = np.empty((num_train,),dtype='uint8')
@@ -32,28 +34,33 @@ def save_image_data():
     x_test = np.empty(testDimensions,dtype='uint8')
     y_test = np.empty((num_test,),dtype='uint8')
 
+    # initialize array used to store each image as RGB
+    rgbPixels = np.empty((channels,scaledHeight,scaledWidth),dtype='uint8')
     # load train dataset (skipping over header row) and convert to list
     sheet = np.loadtxt(train_file_path,dtype=int,delimiter=',',skiprows=1)
     data = sheet.tolist()
     row = 1
     # process each training image in data
     for pic in data:
-        # convert CSV to image and resize
+        # convert CSV to image and scale up, then convert image back to array
         pixels = np.array(pic[1:])
         pixels = np.reshape(pixels,imDimensions)
-        Pixels = Image.fromarray(pixels.astype(np.uint8))
-        scaledPixels = Pixels.resize(scaledDimensions)
-        pixels = np.asarray(scaledPixels)
+        imagePixels = Image.fromarray(pixels.astype(np.uint8))
+        scaledPixels = imagePixels.resize(scaledDimensions)
+        arrayPixels = np.asarray(scaledPixels)
+        # store arrayPixels in rgbPixels to simulate RGB image
+        for i in range(3):
+            rgbPixels[i] = np.reshape(arrayPixels,scaledDimensions)
         # put every 4th label into y_val
-        # reshape image data into 84x84 and store every 4th image in x_val
+        # reshape image data into 3x84x84 and store every 4th image in x_val
         if (row % 4 == 0):
             index = int(row/4 - 1)
             y_val[index] = pic[0]
-            x_val[index] = np.reshape(pixels,scaledDimensions)
+            x_val[index] = rgbPixels
         else:
             index = int(row - ceil(row/4))
             y_train[index] = pic[0]
-            x_train[index] = np.reshape(pixels,scaledDimensions)
+            x_train[index] = rgbPixels
 
         row += 1
     
@@ -63,15 +70,19 @@ def save_image_data():
     row = 0
     # process each image in data
     for pic in data:
+        # convert CSV to image and scale up, then convert image back to array
         pixels = np.array(pic[1:])
         pixels = np.reshape(pixels,imDimensions)
-        Pixels = Image.fromarray(pixels.astype(np.uint8))
-        scaledPixels = Pixels.resize(scaledDimensions)
-        pixels = np.asarray(scaledPixels)
+        imagePixels = Image.fromarray(pixels.astype(np.uint8))
+        scaledPixels = imagePixels.resize(scaledDimensions)
+        arrayPixels = np.asarray(scaledPixels)
+        # store arrayPixels in rgbPixels to simulate RGB image
+        for i in range(3):
+            rgbPixels[i] = np.reshape(arrayPixels,scaledDimensions)
         # put picture label into y_test
         y_test[row] = pic[0]
         # reshape image data into 28x28 and store in x_test
-        x_test[row] = np.reshape(pixels,scaledDimensions)
+        x_test[row] = rgbPixels
         row += 1
 
     return (x_train, y_train), (x_val, y_val), (x_test, y_test)
@@ -96,7 +107,7 @@ def main():
     print("x_val[0] = ",x_val[0])
     print("y_val[0] = ", y_val[0])
     data = ((x_train, y_train), (x_val, y_val), (x_test, y_test))
-    with open('sign_mnist_numpy_scaled_data.pickle','wb') as f:
+    with open('sign_mnist_numpy_scaled_rgb_data.pickle','wb') as f:
         pickle.dump(data,f)
 
 main()
